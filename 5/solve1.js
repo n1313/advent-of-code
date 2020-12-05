e78eb4b63baf1beda577271e2fabaef5576b2b62
Yury Shevchenko <unique.login@gmail.com>
2020-12-05 13:30:55 +0800

    Day 5

---
+++ 5/solve1.js
@@ -0,0 +1,85 @@
+const fs = require('fs');
+const inputString = fs.readFileSync(process.argv[2], 'utf-8').trim().split('\n');
+
+const rows = { from: 0, to: 127 };
+const cols = { from: 0, to: 7 };
+
+main();
+
+function getSeatId(row, col) {
+  return row * 8 + col;
+}
+
+function getHalf(range, which) {
+  const length = range.to - range.from + 1;
+  const upperHalf = {
+    from: range.from,
+    to: range.from + length / 2 - 1,
+  };
+  const lowerHalf = {
+    from: range.to - length / 2 + 1,
+    to: range.to,
+  };
+  // console.log('getHalf', range, upperHalf, lowerHalf, which);
+  if (which === 'F' || which === 'L') {
+    return upperHalf;
+  } else {
+    return lowerHalf;
+  }
+}
+
+function findPosition(letters, range) {
+  // console.log('findPosition', letters, range);
+  if (letters.length > 0) {
+    const firstLetter = letters[0];
+    const remainingLetters = letters.slice(1);
+    const newRange = getHalf(range, firstLetter);
+    return findPosition(remainingLetters, newRange);
+  } else {
+    // console.log('found', letters, range);
+    return range.from;
+  }
+}
+
+function convertPass(pass) {
+  const row = findPosition(pass.rowLetters, rows);
+  const col = findPosition(pass.colLetters, cols);
+  const seatId = getSeatId(row, col);
+  return {
+    ...pass,
+    row,
+    col,
+    seatId,
+  };
+}
+
+function variantDumb(arr) {
+  const passes = arr
+    .map((letters, i) => {
+      return {
+        i,
+        rowLetters: letters.slice(0, 7),
+        colLetters: letters.slice(7),
+      };
+    })
+    .map(convertPass);
+
+  console.log('passes', passes);
+
+  const maxSeatId = passes.reduce((acc, pass) => {
+    return Math.max(pass.seatId, acc);
+  }, -1);
+
+  return maxSeatId;
+}
+
+function solve() {
+  return variantDumb(inputString);
+}
+
+function main() {
+  const start = new Date();
+  let result = solve();
+  console.log(result);
+  console.log('Took', new Date() - start, 'ms');
+}
