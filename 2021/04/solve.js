e4d74e187cf944e42845992ac38a076ad14a4e9d
Yury Shevchenko <unique.login@gmail.com>
2021-12-03 22:34:21 -0800

    Not sure if better but at least its shorter

--- 2021/04/solve.js
+++ 2021/04/solve.js
@@ -15,7 +15,7 @@ const createBoard = (rows) => {
   };
 };
 
-const solver1 = (lines) => {
+const parse = (lines) => {
   const calls = lines[0].split(',').map(Number);
 
   const boards = [];
@@ -29,113 +29,58 @@ const solver1 = (lines) => {
     ]);
     boards.push(board);
   }
+  return [calls, boards];
+};
 
-  let found = false;
-  let x;
-  let b;
+const play = (calls, boards) => {
+  let winner = 0;
 
-  while (!found && calls.length) {
-    x = calls.shift();
-
-    b = 0;
-
-    while (!found && b < boards.length) {
-      const board = boards[b];
-
-      if (board.digits.has(x)) {
+  calls.forEach((x) => {
+    boards.forEach((board) => {
+      if (!board.winner && board.digits.has(x)) {
+        let won = false;
         board.rows = board.rows.map((row) => {
           const newRow = row.filter((c) => c !== x);
           if (newRow.length === 0) {
-            found = true;
+            won = true;
           }
           return newRow;
         });
 
-        board.cols = board.cols.map((col) => {
-          const newCol = col.filter((c) => c !== x);
-          if (newCol.length === 0) {
-            found = true;
-          }
-          return newCol;
-        });
-      }
-      b += 1;
-    }
-  }
-
-  const winner = boards[b - 1];
-  const sum = [].concat(...winner.rows).reduce((a, x) => (a += x), 0);
-
-  return sum * x;
-};
-
-const solver2 = (lines) => {
-  const calls = lines[0].split(',').map(Number);
-
-  const boards = [];
-  for (let i = 2; i < lines.length - 4; i += 6) {
-    const board = createBoard([
-      lineToCells(lines[i]),
-      lineToCells(lines[i + 1]),
-      lineToCells(lines[i + 2]),
-      lineToCells(lines[i + 3]),
-      lineToCells(lines[i + 4]),
-    ]);
-    boards.push(board);
-  }
-
-  let found = false;
-  let x;
-  let b;
-
-  while (!found && calls.length > 0) {
-    x = calls.shift();
-
-    b = 0;
-
-    let stop = false;
-
-    while (!found && b < boards.length) {
-      const board = boards[b];
-
-      if (board) {
-        if (board.digits.has(x)) {
-          board.rows = board.rows.map((row) => {
-            const newRow = row.filter((c) => c !== x);
-            if (newRow.length === 0) {
-              stop = true;
-            }
-            return newRow;
-          });
-
+        if (!won) {
           board.cols = board.cols.map((col) => {
             const newCol = col.filter((c) => c !== x);
             if (newCol.length === 0) {
-              stop = true;
+              won = true;
             }
             return newCol;
           });
         }
 
-        if (stop) {
-          const validBoards = boards.filter(Boolean);
-          if (validBoards.length === 1) {
-            found = true;
-          } else {
-            boards[b] = null;
-            stop = false;
-          }
+        if (won) {
+          board.winner = ++winner;
+          board.x = x;
+          board.sum = [].concat(...board.rows).reduce((a, i) => (a += i), 0);
         }
       }
+    });
+  });
 
-      b += 1;
-    }
-  }
+  return boards;
+};
 
-  const winner = boards[b - 1];
-  const sum = [].concat(...winner.rows).reduce((a, x) => (a += x), 0);
+const solver1 = (lines) => {
+  const [calls, boards] = parse(lines);
+  const postPlayBoards = play(calls, boards);
+  const first = postPlayBoards.find((b) => b.winner === 1);
+  return first.sum * first.x;
+};
 
-  return sum * x;
+const solver2 = (lines) => {
+  const [calls, boards] = parse(lines);
+  const postPlayBoards = play(calls, boards);
+  const last = postPlayBoards.find((b) => b.winner === boards.length);
+  return last.sum * last.x;
 };
 
 const testInput = `7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
