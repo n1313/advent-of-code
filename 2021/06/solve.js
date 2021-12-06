b8f6e6babbbc757abd6002018917dd07a7740684
Yury Shevchenko <unique.login@gmail.com>
2021-12-05 21:39:48 -0800

    Obviously, the second approach is better

--- 2021/06/solve.js
+++ 2021/06/solve.js
@@ -1,51 +1,34 @@
 const solve = require('../../utils/solve');
 
-const EVERY = 7;
-const PENALTY = 2;
-
-const solver1 = (lines) => {
-  const DAYS = 80;
-
-  let fishes = lines[0].split(',');
-
-  for (let day = 0; day < DAYS; day++) {
-    const l = fishes.length;
-    for (let f = 0; f < l; f++) {
-      if (fishes[f] === 0) {
-        fishes[f] = EVERY - 1;
-        fishes.push(EVERY + PENALTY - 1);
-      } else {
-        fishes[f] = fishes[f] - 1;
-      }
-    }
-  }
-
-  return fishes.length;
-};
-
-const solver2 = (lines) => {
-  const DAYS = 256;
+const simulateFish = (fishes, days) => {
   let ages = [0, 0, 0, 0, 0, 0, 0, 0];
 
-  let fishes = lines[0].split(',');
   fishes.forEach((age) => {
     ages[age] += 1;
   });
 
-  for (let day = 0; day < DAYS; day++) {
+  for (let day = 0; day < days; day++) {
     const newAges = [0, 0, 0, 0, 0, 0, 0, 0];
     for (let a = ages.length - 1; a >= 0; a--) {
       if (a > 0) {
         newAges[a - 1] += ages[a];
       } else {
-        newAges[EVERY + PENALTY - 1] = ages[a];
-        newAges[EVERY - 1] += ages[a];
+        newAges[8] = ages[a];
+        newAges[6] += ages[a];
       }
     }
     ages = newAges;
   }
 
   return ages.reduce((a, x) => (a += x), 0);
+};
+
+const solver1 = (lines) => {
+  return simulateFish(lines[0].split(','), 80);
+};
+
+const solver2 = (lines) => {
+  return simulateFish(lines[0].split(','), 256);
 };
 
 const testInput = `3,4,3,1,2`;
