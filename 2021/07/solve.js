25c065045ebda2c1bd85189d83a6da5a720c6ad4
Yury Shevchenko <unique.login@gmail.com>
2021-12-06 22:00:03 -0800

    Huh

--- 2021/07/solve.js
+++ 2021/07/solve.js
@@ -1,37 +1,24 @@
 const solve = require('../../utils/solve');
 
 const solver1 = (lines) => {
-  const positions = lines[0].split(',').sort((a, z) => a - z);
+  const positions = lines[0]
+    .split(',')
+    .map(Number)
+    .sort((a, z) => a - z);
 
-  const min = positions[0];
-  const max = positions[positions.length - 1];
+  const median = positions[positions.length / 2];
 
-  const results = {};
-
-  for (let i = min; i <= max; i++) {
-    const delta = positions.reduce((acc, pos) => (acc += Math.abs(pos - i)), 0);
-    results[i] = delta;
-  }
-
-  return Object.values(results).sort((a, z) => a - z)[0];
+  return positions.reduce((acc, pos) => (acc += Math.abs(pos - median)), 0);
 };
 
 const progDelta = (delta) => (delta + 1) * (delta / 2);
 
 const solver2 = (lines) => {
-  const positions = lines[0].split(',').sort((a, z) => a - z);
+  const positions = lines[0].split(',').map(Number);
 
-  const min = positions[0];
-  const max = positions[positions.length - 1];
+  const mean = Math.round(positions.reduce((a, i) => (a += i), 0) / positions.length);
 
-  const results = {};
-
-  for (let i = min; i <= max; i++) {
-    const delta = positions.reduce((acc, pos) => (acc += progDelta(Math.abs(pos - i))), 0);
-    results[i] = delta;
-  }
-
-  return Object.values(results).sort((a, z) => a - z)[0];
+  return positions.reduce((acc, pos) => (acc += progDelta(Math.abs(pos - mean))), 0);
 };
 
 const testInput = `16,1,2,0,4,2,7,1,2,14`;
