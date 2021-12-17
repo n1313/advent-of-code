39b4b2e1b8abbe421baafc19904fcb21b3c7d546
Yury Shevchenko <unique.login@gmail.com>
2021-12-16 23:41:16 -0800

    A bit better

--- 2021/17/solve.js
+++ 2021/17/solve.js
@@ -1,20 +1,17 @@
 const solve = require('../../utils/solve');
 
-const step = ([posX, posY, speedX, speedY]) => {
-  return [posX + speedX, posY + speedY, speedX > 0 ? speedX - 1 : speedX < 0 ? speedX + 1 : speedX, speedY - 1];
-};
-
 const shoot = (posX, posY, speedX, speedY, targetX, targetY) => {
-  let overshot = false;
   let hit = false;
   let maxY = posY;
-  while (!hit && !overshot) {
-    [posX, posY, speedX, speedY] = step([posX, posY, speedX, speedY]);
+  while (posX <= targetX[1] && posY >= targetY[0]) {
+    posX += speedX;
+    posY += speedY;
+    speedX -= Math.sign(speedX);
+    speedY -= 1;
     maxY = Math.max(posY, maxY);
     if (posX >= targetX[0] && posX <= targetX[1] && posY <= targetY[1] && posY >= targetY[0]) {
       hit = true;
-    } else if (posX > targetX[1] || posY < targetY[0]) {
-      overshot = true;
+      break;
     }
   }
   return [hit, maxY];
@@ -63,7 +60,7 @@ const solver2 = (lines) => {
 
   for (let speedX = minSpeedX; speedX <= maxSpeedX; speedX++) {
     for (let speedY = minSpeedY; speedY <= maxSpeedY; speedY++) {
-      const [hit, maxY] = shoot(0, 0, speedX, speedY, targetX, targetY);
+      const [hit] = shoot(0, 0, speedX, speedY, targetX, targetY);
       if (hit) {
         count += 1;
       }
